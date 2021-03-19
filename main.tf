a28716aeba40de51e9426bbf8f2457d1b2157a5a
Poorani Kamalakannan - (LA ID) <pooranikamalakannan@allianzlife.com>
2021-03-19 09:37:10 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,14 @@
-terraform{
-    required_providers{
-        azurerm={
-            source = "hashicorp/azurerm"
-            version=">=0.1"
-        }
 
-    }
-}
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x. 
+  # If you are using version 1.x, the "features" block is not allowed.
 
-provider "azurerm" { 
   features {}
 }
-
-
+terraform {
+  backend "azurerm" {
+  }
+}
 
 #Create WebAp
 module "webapp" {
