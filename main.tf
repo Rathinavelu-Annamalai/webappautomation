2eec2b8666e821992cffb258b3951bcd608b8c57
Poorani Kamalakannan - (LA ID) <pooranikamalakannan@allianzlife.com>
2021-03-19 08:32:14 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -2,28 +2,14 @@ provider "azurerm" {
   features {}
 }
 
-#create resource group
-resource "azurerm_resource_group" "rg" {
-    name     = "rg-MyFirstTerraform"
-    location = "westus"
-}
-
-#Create Storage Account
-module "storage_account" {
-  source    = "./modules/storage-account"
-
-  saname    = "stathcldemo21"
-  rgname    = azurerm_resource_group.rg.name
-  location  = azurerm_resource_group.rg.location
-}
 
 
 #Create WebAp
 module "webapp" {
   source    = "./modules/webapp"
 
-  appsplanname    = "appserplanvhcl2190"
-appsname="appservhcl2190"
+  appsplanname    = "appserplanvhcl90"
+appsname="appservhcl90"
   rgname    = azurerm_resource_group.rg.name
   location  = azurerm_resource_group.rg.location
 }
