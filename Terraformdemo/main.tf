78b569928838004458310ded39362d6b4aa56140
Poorani Kamalakannan - (LA ID) <pooranikamalakannan@allianzlife.com>
2021-03-19 09:56:17 +0000

    Updated main.tf

--- Terraformdemo/main.tf
+++ Terraformdemo/main.tf
@@ -1,1 +1,17 @@
-ddfd
+
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x. 
+  # If you are using version 1.x, the "features" block is not allowed.
+version = ">=2.0"
+features{}
+  
+}
+#Create WebAp
+module "webapp" {
+  source    = "./modules/webapp"
+
+  appsplanname    = "appserplanvhcl90"
+appsname="appservhcl90"
+  rgname    = azurerm_resource_group.rg.name
+  location  = azurerm_resource_group.rg.location
+}
