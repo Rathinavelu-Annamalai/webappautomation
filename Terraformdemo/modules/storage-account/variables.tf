fbf58491bb386783742c19bdc0644e1fea843cb5
Poorani Kamalakannan - (LA ID) <pooranikamalakannan@allianzlife.com>
2021-03-19 10:25:25 +0000

    Added variables.tf

---
+++ Terraformdemo/modules/storage-account/variables.tf
@@ -0,0 +1,13 @@
+variable "saname"{ 
+    type = string
+    description = "Name of the storage account"
+}
+variable "rgname" {
+    type = string
+    description = "Name of resource group"
+}
+variable "location" {
+    type = string
+    description = "Azure location of storage account environment"
+    default = "westus2"
+}
