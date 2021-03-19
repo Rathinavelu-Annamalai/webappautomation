ee2330edef501fbc7ca54527b863c77ef2a33f7d
Poorani Kamalakannan - (LA ID) <pooranikamalakannan@allianzlife.com>
2021-03-19 10:26:56 +0000

    Updated variables.tf

--- Terraformdemo/modules/webapp/variables.tf
+++ Terraformdemo/modules/webapp/variables.tf
@@ -2,14 +2,18 @@ variable "appsplanname"{
     type = string
     description = "Name of the app service plan name"
 }
-
 variable "rgname" {
     type = string
     description = "Name of resource group"
-    default="griffin-resource-group"
+}
+variable "location" {
+    type = string
+    description = "Azure location of storage account environment"
+    default = "westus2"
 }
 
 variable "appsname"{
     type = string
     description = "Name of the app service plan name"
 }
+
