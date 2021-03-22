28e611dd7ed7b3b26b0433ab33d3538c1ded0e41
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-03-22 08:08:49 +0000

    Updated main.tf

--- app/main.tf
+++ app/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 provider "azurerm" {
  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
- version         = "=2.18.0"
+ //version         = "=2.18.0"
  //subscription_id = var.acr_subscription_id
  features {}
  skip_provider_registration = "true"
