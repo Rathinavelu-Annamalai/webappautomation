a10628bcd266f25ae61ddb6aaf3cb96df6ee2423
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-03-23 08:17:44 +0000

    Added variables.tf

---
+++ app/variables.tf
@@ -0,0 +1,10 @@
+
+
+variable "subscription_id" {
+  description = "Azure Subscription ID to be used for billing"
+}
+
+variable "my_sql_master_password" {
+  description = "MySql master password"
+}
+
