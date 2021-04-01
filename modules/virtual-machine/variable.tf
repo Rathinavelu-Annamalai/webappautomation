9f90a0192253c4a88cd977b9e884df256c46b36b
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-01 13:45:54 +0000

    Added variable.tf

---
+++ modules/virtual-machine/variable.tf
@@ -0,0 +1,35 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default="demo"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default="northeurope"
+  
+}
+
+variable "resource_group_name" {
+  description = "Default resource group name that the network will be created in."
+  default     = "demo-rg"
+}
+
+variable "username" {
+  description = "User Name"
+  default="sampath"
+  
+}
+
+variable "password" {
+  description = "password"
+  default="sampath@123"
+  
+}
+
+variable "vm_count" {
+default= ["1","2"]
+}
+
+variable "server_zones" {
+default=["1","2"]
+}
