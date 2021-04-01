8ea26bb2ab4c27c130e0f3cc00d13c74088165e1
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-01 10:22:27 +0000

    Updated variable.tf

--- Resource-Provisioning/variable.tf
+++ Resource-Provisioning/variable.tf
@@ -1,33 +1,31 @@
 variable "vnet_name" {
   description = "Name of the vnet to create"
-  default     = ""
+  default     = "my-demo-vnet"
 }
 
 variable "resource_group_name" {
   description = "Default resource group name that the network will be created in."
-  default     = ""
+  default     = "my-demo-rg"
 }
 
 variable "location" {
   description = "The location/region where the core network will be created."
-  default     = ""
+  default     = "central us"
 }
 
 variable "address_space" {
   description = "The address space that is used by the virtual network."
-  // default     = "["10.0.0.0/16"] "
-    default = ""
+   default     = "["10.0.0.0/16"]"
 }
+
 variable "subnet_prefixes" {
   description = "The address prefix to use for the subnet."
-  //default     = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
-  default = ""
+  default     = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
 }
 
 variable "subnet_names" {
   description = "A list of public subnets inside the vNet."
-  //default     = ["subnet1", "subnet2", "subnet3"]
-  default= ""
+  default     = ["subnet1", "subnet2", "subnet3"]
 }
 
 
