353cbfd9fd21313831f2af50054acef4cddd1fe2
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-01 13:12:32 +0000

    Updated variable.tf

--- modules/network/variable.tf
+++ modules/network/variable.tf
@@ -1,26 +1,14 @@
-variable "virtual_network_name" {
-  description = "Name of the vnet to create"
-  }
+variable "virtual_network_name" {}
+  
+variable "resource_group_name" {}
 
-variable "resource_group_name" { 
-  description = "Default resource group name that the network will be created in."
-}
-
-variable "location" {
-  description = "The location/region where the core network will be created."
-}
-
-variable "virtual_network_address_space" {
-  description = "The address space that is used by the virtual network."
-}
-
-variable "subnet_prefixes" {
-  description = "The address prefix to use for the subnet."
-}
-
-variable "subnet_names" {
-  description = "A list of public subnets inside the vNet."
- }
+variable "location" {}
+  
+variable "virtual_network_address_space" {}
+  
+variable "subnet_prefixes" {}
+  
+variable "subnet_names" {}
 
 
 
