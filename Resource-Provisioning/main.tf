f6e65e7fd1a7a703257877798271990d93ce399f
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-01 10:45:58 +0000

    Updated main.tf

--- Resource-Provisioning/main.tf
+++ Resource-Provisioning/main.tf
@@ -8,3 +8,19 @@ terraform {
   backend "azurerm" {
   }
 }
+
+
+
+module "network" {
+source = "../modules/network"
+//name                = var.virtual_network_name
+location            = var.location
+address_space       = var.virtual_network_address_space
+resource_group_name = var.resource_group_name
+name                      = var.subnet_names[count.index]
+virtual_network_name      = var.virtual_network_name
+resource_group_name       = var.resource_group_name
+address_prefix            = var.subnet_prefixes[count.index]
+count                     = length(var.subnet_names)
+
+}
