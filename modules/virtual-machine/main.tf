b4bb5d52f2368853a4ec74d38baa4f5577a8f200
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-02 15:30:59 +0000

    Updated main.tf

--- modules/virtual-machine/main.tf
+++ modules/virtual-machine/main.tf
@@ -1,21 +1,28 @@
+module "rg" {
+ source= "./modules/resourcegroup"
+ name=var.resource_group_name
+ location=var.location
 
+}
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/22"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+module "vnet" {
+  source="./modules/vnet"
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = module.rg.location
+  resource_group_name = module.rg.name
 } 
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-} 
+module "snet" {
+source= "./modules/subnet"
+  virtual_network_name      = module.vnet.vnet_name
+  resource_group_name       = module.rg.name
+  address_prefix            = "10.0.1.0/24"
+  //count                     = length(var.subnet_names)
+}
 
 
-/*resource "azurerm_availability_set" "vm" {
+ /*resource "azurerm_availability_set" "vm" {
   name                         = "${var.prefix}-avset"
   location                     = azurerm_resource_group.main.location
   resource_group_name          = azurerm_resource_group.main.name
@@ -26,30 +33,30 @@ resource "azurerm_subnet" "internal" {
 
 
 resource "azurerm_network_interface" "main" {
-  count=length(var.vm_count)
-  name                = var.prefix-nic-"${var.vm_count[count.index]}"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  //count=length(var.vm_count)
+  name                = module.rg.name-vnet
+  resource_group_name = module.rg.name
+  location            = module.rg.location
 
   ip_configuration {
-    name                          = internal-${var.vm_count[count.index]}
-    subnet_id                     = azurerm_subnet.internal.id
+    name                          = internal
+    subnet_id                     = module.subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
   
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
-  name                            = "${var.prefix}-vm-"${var.vm_count[count.index]}""
-  resource_group_name             = azurerm_resource_group.main.name
+  name                            = module.rg.name-vm
+  resource_group_name             = module.rg.name
  // availability_set_id             = azurerm_availability_set.vm.id
-  zone= var.server_zones[count.index]
-  location                        = azurerm_resource_group.main.location
+  //zone= var.server_zones[count.index]
+  location                        =module.rg.location
   size                            = "Standard_D2s_v3"
   admin_username                  = var.username
   admin_password                  = var.password
   disable_password_authentication = false
-  count=length(var.vm_count)
+  //count=length(var.vm_count)
    network_interface_ids = [
   azurerm_network_interface.main.id,
   ]
