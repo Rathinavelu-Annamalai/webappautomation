008e2428ec43cabb445b2a657a7ba97ca0f51fbb
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-05 12:03:15 +0000

    Updated variable.tf

--- Resource-Provisioning/variable.tf
+++ Resource-Provisioning/variable.tf
@@ -16,7 +16,7 @@ variable "location" {
 
 variable "address_space" {
   description = "The address space that is used by the virtual network."
-   default     = "["10.0.0.0/16"]"
+   default     = ["10.0.0.0/16"]
 }
 
 variable "subnet_prefixes" {
