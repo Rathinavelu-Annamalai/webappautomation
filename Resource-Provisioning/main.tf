fe52e9df7712df2b0974bc2913600c60e0b0c8f7
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-05 14:07:23 +0000

    Updated main.tf

--- Resource-Provisioning/main.tf
+++ Resource-Provisioning/main.tf
@@ -22,14 +22,14 @@ terraform {
 
 module "rg" {
 source= "../modules/resourcegroup"
-resource_group_name=var.resource_group_name
+name=var.resource_group_name
 location=var.location
 
 }
 
 module "application-vnet" {
   source              = "../modules/vnet"
-  resource_group_name =  module.rg.resource_group_name
+  resource_group_name =  module.rg.name
   location            = module.rg.location
   vnet_name           = var.vnet_name
   address_space       = var.address_space
@@ -43,7 +43,7 @@ module "application-vnet" {
 module "subnets" {
 source                    = "../modules/subnet"
 //name                      = var.subnet_names[count.index]
-subnet_names                = var.subnet_names
+name               = var.subnet_names
  // virtual_network_name      = module.application-vnet.virtual_network_name
  vnet_name      = module.application-vnet.vnet_name
 resource_group_name       = data.azurerm_resource_group.rg.name
