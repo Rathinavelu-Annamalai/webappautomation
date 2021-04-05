a5397a79da3334fc6169da9d27c7746a75ce3874
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-05 10:34:33 +0000

    Updated main.tf

--- modules/resourcegroup/main.tf
+++ modules/resourcegroup/main.tf
@@ -1,17 +1,13 @@
- terraform {
-  required_version = ">= 0.11" 
- backend "azurerm" {
-  storage_account_name = "__terraformstorageaccount__"
-    container_name       = "terraform"
-    key                  = "terraform.tfstate"
-	access_key  ="__storagekey__"
-  features{}
-	}
-	}
-  provider "azurerm" {
-    version = "=2.0.0"
-features {}
+provider "azurerm" {
+  features {}
 }
+/*
+data "azurerm_resource_group" "example" {
+  name     = "appdbwebtest"
+  location = "Central US"
+
+ 
+}*/
 
 resource "resource_group_name" "rg"{
     name=var.resource_group_name
