90abb39424aea962c012cce77a0163abae9aef9a
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-05 14:00:04 +0000

    Updated main.tf

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "vnet" {
   resource_group_name = "${var.resource_group_name}"
   location            = "${var.location}"
-  vnet_name                = "${var.vnet_name}"
+  name                = "${var.vnet_name}"
   address_space       = ["${var.address_space}"]
   dns_servers         = "${var.dns_servers}"
   tags                = "${var.tags}"
