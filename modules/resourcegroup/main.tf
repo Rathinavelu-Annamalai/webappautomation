bb56782e20cc873452529a65b41bdae6d470e97b
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-05 10:04:24 +0000

    Updated main.tf

--- modules/resourcegroup/main.tf
+++ modules/resourcegroup/main.tf
@@ -16,5 +16,11 @@ features {}
 resource "resource_group_name" "rg"{
     name=var.resource_group_name
     location=var.location
-    tags=var.tags
+    tags=
+    {
+      CreatedBy="sampath"
+      Environment="dev"
+      ModeOfDeployment="cicd"
+    
+    }
 }
