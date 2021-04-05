fc3975948e4ba93b02b1e5a8cead3c16b06b9487
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-05 10:43:07 +0000

    Updated variable.tf

--- modules/resourcegroup/variable.tf
+++ modules/resourcegroup/variable.tf
@@ -1,9 +1,9 @@
 variable "resource_group_name" {
   description = "The name of the resource group we want to use"
-  default     = "my-demo-group1"
+  default     = ""
 }
 
 variable "location" {
   description = "The location/region where we are crrating the resource"
-  default     = "Central US"
+  default     = ""
 }
