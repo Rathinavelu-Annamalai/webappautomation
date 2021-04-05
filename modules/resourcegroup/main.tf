dd369e23a71d95a2982506d8e12f040b79f391a8
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-05 07:59:19 +0000

    Updated main.tf

--- modules/resourcegroup/main.tf
+++ modules/resourcegroup/main.tf
@@ -1,12 +1,6 @@
- terraform {
-  required_version = ">=0.11" 
- backend "azurerm" {
-  
-	}
-	}
-  provider "azurerm" {
-    version = ">=0.11"
-features {}
+provider "azurerm" {
+ version = "=2.0.0"
+ features { }  
 }
 
 resource "resource_group_name" "rg"{
