9816d7c15afbef32498f0db43b7db0ed4b71b03b
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-05 15:17:00 +0000

    Updated variable.tf

--- Resource-Provisioning/variable.tf
+++ Resource-Provisioning/variable.tf
@@ -20,18 +20,18 @@ variable "address_space" {
    default     = "10.0.0.0/16"
 }
 
-variable "subnet_prefix" {
+variable "subnet_prefixes" {
   description = "The address prefix to use for the subnet."
-  // default     = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
-  type = string
-   default= "10.0.1.0/24"
+  type = list(string)
+   default     = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+     
 }
 
-variable "subnet_name" {
+variable "subnet_namees" {
   description = "A list of public subnets inside the vNet."
-  // default     = ["subnet1", "subnet2", "subnet3"]
-  type = string
-   default="subnet1"
+  type = list(string)
+  default     = ["subnet1", "subnet2", "subnet3"]
+    
 }
 
 variable "tags" {
