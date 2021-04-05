dc1af2a45436d057f9b5f97b0dac25c72be1ac55
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-05 12:18:01 +0000

    Updated variable.tf

--- modules/resourcegroup/variable.tf
+++ modules/resourcegroup/variable.tf
@@ -1,6 +1,6 @@
 variable "resource_group_name" {
   description = "The name of the resource group we want to use"
-  default     = "demorg21"
+  default     = "demorg2321"
 }
 
 variable "location" {
