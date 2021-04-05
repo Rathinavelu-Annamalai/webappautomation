2b5baa26fff5bb88b9a646e2accd82e4e04e3f3d
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-05 07:09:46 +0000

    Updated main.tf

--- modules/resourcegroup/main.tf
+++ modules/resourcegroup/main.tf
@@ -1,12 +1,6 @@
-terraform {
-required_providers {
-azurerm = {
-source  = "hashicorp/azurerm"
-version = "~> 2.43.0"
-}
-}
-}
-   
+provider "azurerm" {
+  features {}
+}   
 
 
 resource "resource_group_name" "rg"{
