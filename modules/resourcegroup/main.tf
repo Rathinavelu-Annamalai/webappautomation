30d03172c4e118b590e525a38856c93084587e6d
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-05 11:26:12 +0000

    Updated main.tf

--- modules/resourcegroup/main.tf
+++ modules/resourcegroup/main.tf
@@ -2,22 +2,13 @@ provider "azurerm" {
   version = "=2.33.0"
   features {}
 }
-/*
-data "azurerm_resource_group" "example" {
-  name     = "appdbwebtest"
-  location = "Central US"
-
- 
-}*/
 
 resource "azurerm_resource_group" "rg"{
     name=var.resource_group_name
     location=var.location
     tags= {
-
       CreatedBy="sampath"
       Environment="dev"
-      ModeOfDeployment="cicd"
-    
+      ModeOfDeployment="cicd"    
     }
 }
