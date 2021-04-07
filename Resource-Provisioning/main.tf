c08c27fc2f2d1040b027eeb5a3f3ee3f2d1750e1
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-07 12:47:38 +0000

    Added 2 files to /Resource-Provisioning

---
+++ Resource-Provisioning/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 0.12.0"
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "${var.application}-${var.environment}"
+  location = "${var.location}"
+  tags     = "${merge(var.default_tags, map("type", "resource"))}"
+}
+
+module "application-vnet" {
+  source              = "./modules/vnet"
+  resource_group_name = "${azurerm_resource_group.resource_group.name}"
+  location            = "${var.location}"
+  tags                = "${merge(var.default_tags, map("type", "network"))}"
+  vnet_name           = "${azurerm_resource_group.resource_group.name}-vnet"
+  address_space       = "${var.address_space}"
+}
+
+module "application-subnets" {
+  source              = "./modules/subnet"
+  resource_group_name = "${azurerm_resource_group.resource_group.name}"
+  location            = "${var.location}"
+  tags                = "${merge(var.default_tags, map("type", "network"))}"
+  vnet_name           = "${module.application-vnet.vnet_name}"
+
+  subnets = [
+    {
+      name   = "${azurerm_resource_group.resource_group.name}-subnet"
+      prefix = "${var.subnet}"
+    }
+  ]
+}
---
+++ Resource-Provisioning/variables.tf
@@ -0,0 +1,35 @@
+variable "resource_group_name" {
+  description = ""
+  default     = ""
+}
+
+variable "location" {
+  description = ""
+  default     = ""
+}
+
+variable "default_tags" {
+  description = ""
+  type        = "map"
+  default     = {}
+}
+
+variable "address_space" {
+  description = ""
+  default     = ""
+}
+
+variable "subnet" {
+  description = ""
+  default     = ""
+}
+
+variable "application" {
+  description = ""
+  default     = ""
+}
+
+variable "environment" {
+  description = ""
+  default     = ""
+}
