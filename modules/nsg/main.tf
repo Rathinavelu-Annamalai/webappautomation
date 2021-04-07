48ed9a4c05e8d81d796b30440af54a6e9a738896
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-07 14:26:55 +0000

    Updated main.tf

--- modules/nsg/main.tf
+++ modules/nsg/main.tf
@@ -1,5 +1,7 @@
 resource "azurerm_network_security_group" "example" {
-  name                = "example-nsg"
+  //name                = "example-nsg"
+  name="${var.nsg[count.index]}"
+  count=length(var.nsg_name)
   //location            = azurerm_resource_group.dev.location
   location            = "${var.location}"
   //resource_group_name = "${azurerm_resource_group.dev.name}""
