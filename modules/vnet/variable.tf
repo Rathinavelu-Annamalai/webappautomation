4bb697d290c1a8486af3b502f154d74b6061c3be
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-07 06:43:11 +0000

    Updated variable.tf

--- modules/vnet/variable.tf
+++ modules/vnet/variable.tf
@@ -22,7 +22,8 @@ variable "vnet_name" {
 
 variable "address_space" {
   description = "The address space that is used by the virtual network."
-  default     = ""
+  default     = ["10.0.0.0/16"]
+
 }
 
 variable "dns_servers" {
