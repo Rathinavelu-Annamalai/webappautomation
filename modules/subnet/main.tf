e1558d8e3b4f2d26b7c3c3194cc38d075a2e0623
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-07 13:40:05 +0000

    Updated main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,12 +1,11 @@
 resource "azurerm_subnet" "subnet" {
-  
-  resource_group_name  = var.resource_group_name
-  name                 = var.subnet_names[count.index]
-  virtual_network_name = var.vnet_name
-   address_prefixes            = [var.subnet_prefixes[count.index]]
-   count              = length(var.subnet_names)
+  count                = "${var.add_endpoint != true ? length(var.subnets) : 0}"
+  resource_group_name  = "${var.resource_group_name}"
+  name                 = "${lookup(var.subnets[count.index], "name", "")}"
+  virtual_network_name = "${var.vnet_name}"
+  address_prefixes       = "${lookup(var.subnets[count.index], "prefix", "")}"
 }
-/*
+
 resource "azurerm_subnet" "subnet-endpoint" {
   count                = "${var.add_endpoint == true ? length(var.subnets) : 0}"
   resource_group_name  = "${var.resource_group_name}"
@@ -14,4 +13,4 @@ resource "azurerm_subnet" "subnet-endpoint" {
   virtual_network_name = "${var.vnet_name}"
   address_prefixes       = "${lookup(var.subnets[count.index], "prefix", "")}"
   service_endpoints    = ["${lookup(var.subnets[count.index], "service_endpoint", "")}"]
-}*/
+}
