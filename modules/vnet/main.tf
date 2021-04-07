3c66ec9123204a4dbaa35f85782374134197256f
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-07 12:55:22 +0000

    Updated main.tf

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -4,5 +4,9 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "${var.vnet_name}"
   address_space       = ["${var.address_space}"]
   dns_servers         = "${var.dns_servers}"
-  tags                = "${var.tags}"
+  tags= {
+      CreatedBy="sampath"
+      Environment="dev"
+      ModeOfDeployment="cicd"    
+    }
 }
