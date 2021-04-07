c6bf2636d0d9c4a5d1cfd8c2fe4edbdcbf13dec7
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-07 14:26:18 +0000

    Updated main.tf

--- Resource-Provisioning/main.tf
+++ Resource-Provisioning/main.tf
@@ -37,3 +37,10 @@ module "application-subnets" {
     }
   ]
 }
+
+  
+  resource "azurerm_subnet_network_security_group_association" "example" {
+  source= "../modules/nsg"
+  subnet_id                 = "${module.application-subnets.subnet_id}"
+  network_security_group_id = azurerm_network_security_group.example.id
+}
