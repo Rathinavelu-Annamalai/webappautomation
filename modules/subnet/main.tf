56465368ad8b234aac805b288288d73f3da13422
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-08 10:42:25 +0000

    Updated main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,10 +1,18 @@
-resource "azurerm_subnet" "subnet" {
+/*resource "azurerm_subnet" "subnet" {
   count                = "${var.add_endpoint != true ? length(var.subnets) : 0}"
   resource_group_name  = "${var.resource_group_name}"
   name                 = "${lookup(var.subnets[count.index], "name", "")}"
   virtual_network_name = "${var.vnet_name}"
   address_prefix       = "${lookup(var.subnets[count.index], "prefix", "")}"
-}
+} */
+
+resource "azurerm_subnet" "subnet" {
+  resource_group_name  = "${var.resource_group_name}"
+  name                 = "${var.subnet_name}"
+  virtual_network_name = "${var.vnet_name}"
+  address_prefix       = "${var.subnet_prefix}"
+} 
+
 
 /*resource "azurerm_subnet" "subnet-endpoint" {
   count                = "${var.add_endpoint == true ? length(var.subnets) : 0}"
@@ -52,7 +60,6 @@ resource "azurerm_network_security_group" "example" {
 
   
   resource "azurerm_subnet_network_security_group_association" "example" {
-  count                = "${var.add_endpoint != true ? length(var.subnets) : 0}"
-  subnet_id                 = "${azurerm_subnet.subnet.0.id}"
+    subnet_id                 = "${azurerm_subnet.subnet.id}"
   network_security_group_id = azurerm_network_security_group.example.id
 } 
