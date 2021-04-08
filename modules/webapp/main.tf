a05c3592b2e8d44d752a37f7cba55ae65c6f4e93
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-08 09:30:57 +0000

    Updated main.tf

---
+++ modules/webapp/main.tf
@@ -0,0 +1,43 @@
+resource "azurerm_app_service_plan" "dev" {
+	  name                = "__appserviceplan__"
+	  //location            = "Central US" 
+	  location="${azurerm_resource_group.dev.location}"
+	  //resource_group_name = "appdbwebtest"
+	resource_group_name="${azurerm_resource_group.dev.name}"
+	
+	  sku {
+	    tier = "Premiumv2"
+	    size = "P1v2"
+	  }
+	    tags = {
+	    environment = "dev"
+	    createdby="poorani"
+	    modeofdeployment= "azurecicd"
+	  }
+	
+	}
+	resource "azurerm_app_service" "dev" {
+	  name                = "__appservicename__"
+	  location            = azurerm_app_service_plan.dev.location
+	  resource_group_name =  azurerm_app_service_plan.dev.resource_group_name
+	  app_service_plan_id = "${azurerm_app_service_plan.dev.id}"
+	  depends_on = [azurerm_app_service_plan.dev]
+      //, azurerm_monitor_autoscale_setting.asplan1]
+	  /*app_settings = {
+	    "APPINSIGHTS_INSTRUMENTATIONKEY" = "${azurerm_application_insights.example.instrumentation_key}",
+	    "WEBSITE_DNS_SERVER": "168.63.129.16",
+	    "WEBSITE_VNET_ROUTE_ALL": "1"
+	  }*/
+	
+	    tags = {
+	    environment = "dev"
+	    createdby="poorani"
+	    modeofdeployment= "azurecicd"
+	  }
+	
+	}
+	resource "azurerm_app_service_virtual_network_swift_connection" "vnetintegrationconnection" {
+	  app_service_id  = azurerm_app_service.dev.id
+	  subnet_id       = azurerm_subnet.integrationsubnet.id
+	}
+	
