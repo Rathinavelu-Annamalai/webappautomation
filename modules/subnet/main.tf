9024c77413a110345ca8e6ebb88950fc3e183dd5
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-08 11:30:00 +0000

    Updated main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,11 +1,18 @@
-resource "azurerm_subnet" "subnet" {
+/*resource "azurerm_subnet" "subnet" {
   count                = "${var.add_endpoint != true ? length(var.subnets) : 0}"
   resource_group_name  = "${var.resource_group_name}"
   name                 = "${lookup(var.subnets[count.index], "name", "")}"
   virtual_network_name = "${var.vnet_name}"
   address_prefix      = "${lookup(var.subnets[count.index], "prefix", "")}"
-}
+}*/
 
+resource "azurerm_subnet" "subnet" {
+  //count                = "${var.add_endpoint != true ? length(var.subnets) : 0}"
+  resource_group_name  = "${var.resource_group_name}"
+  name                 = "${var.subnet_name}""
+  virtual_network_name = "${var.vnet_name}"
+  address_prefix      = "${var.subnet_prefix}"
+}
 resource "azurerm_subnet" "subnet-endpoint" {
   count                = "${var.add_endpoint == true ? length(var.subnets) : 0}"
   resource_group_name  = "${var.resource_group_name}"
