50a9a5ae1681a6bc02179cdb47bcafab1bc5692a
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-08 08:12:42 +0000

    Updated main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -3,17 +3,45 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = "${var.resource_group_name}"
   name                 = "${lookup(var.subnets[count.index], "name", "")}"
   virtual_network_name = "${var.vnet_name}"
-  address_prefixes       = "${lookup(var.subnets[count.index], "prefix", "")}"
+  address_prefix      = "${lookup(var.subnets[count.index], "prefix", "")}"
 }
 
-/*resource "azurerm_subnet" "subnet-endpoint" {
+resource "azurerm_subnet" "subnet-endpoint" {
   count                = "${var.add_endpoint == true ? length(var.subnets) : 0}"
   resource_group_name  = "${var.resource_group_name}"
   name                 = "${lookup(var.subnets[count.index], "name", "")}"
   virtual_network_name = "${var.vnet_name}"
   address_prefix       = "${lookup(var.subnets[count.index], "prefix", "")}"
   service_endpoints    = ["${lookup(var.subnets[count.index], "service_endpoint", "")}"]
-}*/
+}
+resource "azurerm_network_security_group" "example" {
+	  name                = "example-nsg"
+	  location            = azurerm_resource_group.dev.location
+	  resource_group_name = azurerm_resource_group.dev.name
+	
+	  security_rule {
+	    name                       = "test123"
+	    priority                   = 100
+	    direction                  = "Inbound"
+	    access                     = "Allow"
+	    protocol                   = "Tcp"
+	    source_port_range          = "*"
+	    destination_port_range     = "*"
+	    source_address_prefix      = "*"
+	    destination_address_prefix = "*"
+	  }
+	   tags = {
+	   environment = "dev"
+	   createdby="poorani"
+	   modeofdeployment= "azurecicd"
+	  }
+	}
+	
+	resource "azurerm_subnet_network_security_group_association" "example" {
+	  subnet_id                 = azurerm_subnet.integrationsubnet.id
+	  network_security_group_id = azurerm_network_security_group.example.id
+	}
+	
 
 /*resource "azurerm_network_security_group" "example" {
   name                = "example-nsg"
