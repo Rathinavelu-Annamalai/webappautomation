8d4f958cbea694cf77c9fafee15479917ba83d78
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-08 11:03:56 +0000

    Updated output.tf

--- modules/subnet/output.tf
+++ modules/subnet/output.tf
@@ -1,17 +1,17 @@
 output "vnet_subnets" {
   description = "The ids of subnets created inside the new vNet"
   //value       = "${azurerm_subnet.subnet.0.id}"
-  value       = "${azurerm_subnet.subnet.0.id}"
+  value       = "${azurerm_subnet.subnet.id}"
 }
 
 output "vnet_subnet_names" {
   description = "The ids of subnets created inside the new vNet"
   //value       = "${flatten(concat(azurerm_subnet.subnet.*.name, azurerm_subnet.subnet-endpoint.*.name))}"
-  value       = "${azurerm_subnet.subnet.*.name}"
+  value       = "${azurerm_subnet.subnet.name}"
 }
 
 output "subnet" {
   description = "The ids of subnets created inside the new vNet"
   //value       = "${azurerm_subnet.subnet.0.id}"
-  value       = "${azurerm_subnet.subnet.0.id}"
+  value       = "${azurerm_subnet.subnet.id}"
 }
