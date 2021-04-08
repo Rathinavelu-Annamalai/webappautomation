cdd20bb2e00c23fa05b6cf68a5afa8e240e1c281
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-08 10:10:04 +0000

    Updated variable.tf

--- modules/webapp/variable.tf
+++ modules/webapp/variable.tf
@@ -6,3 +6,12 @@ variable "appsname"{
     type=string
     description= "Name of the app service"
 }
+variable "resource_group_name" {
+  description = "The name of the resource group we want to use"
+  default     = ""
+}
+
+variable "location" {
+  description = "The location/region where we are creating the resource"
+  default     = ""
+}
