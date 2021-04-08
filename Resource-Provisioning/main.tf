481139bee10524795902ef74cee62f7b41822855
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-08 10:59:40 +0000

    Updated main.tf

--- Resource-Provisioning/main.tf
+++ Resource-Provisioning/main.tf
@@ -33,7 +33,7 @@ module "application-webapp" {
   
 }
 resource "azurerm_app_service_virtual_network_swift_connection" "vnetintegrationconnection" {
-	  app_service_id  = "${module.application-webapp.subnet_id}"
+	  app_service_id  = "${module.application-webapp.id}"
         subnet_id="${module.application-subnets.subnet_id}"
       
 	}
