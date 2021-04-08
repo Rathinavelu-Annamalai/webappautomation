d07441acbc20d384700b81db6ef1bddd371fa930
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-08 11:44:46 +0000

    Updated main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -13,14 +13,14 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = "${var.vnet_name}"
   address_prefix      = "${var.subnet_prefix}"
 }
-resource "azurerm_subnet" "subnet-endpoint" {
+/*resource "azurerm_subnet" "subnet-endpoint" {
   count                = "${var.add_endpoint == true ? length(var.subnets) : 0}"
   resource_group_name  = "${var.resource_group_name}"
   name                 = "${lookup(var.subnets[count.index], "name", "")}"
   virtual_network_name = "${var.vnet_name}"
   address_prefix       = "${lookup(var.subnets[count.index], "prefix", "")}"
   service_endpoints    = ["${lookup(var.subnets[count.index], "service_endpoint", "")}"]
-}
+}*/
 
 /*resource "azurerm_network_security_group" "example" {
   name                = "example-nsg"
