6cc08c90e011a790855627eee7b663e37f6866d0
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-08 08:17:37 +0000

    Updated main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -13,35 +13,6 @@ resource "azurerm_subnet" "subnet-endpoint" {
   virtual_network_name = "${var.vnet_name}"
   address_prefix       = "${lookup(var.subnets[count.index], "prefix", "")}"
   service_endpoints    = ["${lookup(var.subnets[count.index], "service_endpoint", "")}"]
-}
-resource "azurerm_network_security_group" "example" {
-	  name                = "example-nsg"
-	  location            = azurerm_resource_group.dev.location
-	  resource_group_name = azurerm_resource_group.dev.name
-	
-	  security_rule {
-	    name                       = "test123"
-	    priority                   = 100
-	    direction                  = "Inbound"
-	    access                     = "Allow"
-	    protocol                   = "Tcp"
-	    source_port_range          = "*"
-	    destination_port_range     = "*"
-	    source_address_prefix      = "*"
-	    destination_address_prefix = "*"
-	  }
-	   tags = {
-	   environment = "dev"
-	   createdby="poorani"
-	   modeofdeployment= "azurecicd"
-	  }
-	}
-	
-	resource "azurerm_subnet_network_security_group_association" "example" {
-	  subnet_id                 = azurerm_subnet.integrationsubnet.id
-	  network_security_group_id = azurerm_network_security_group.example.id
-	}
-	
 
 /*resource "azurerm_network_security_group" "example" {
   name                = "example-nsg"
