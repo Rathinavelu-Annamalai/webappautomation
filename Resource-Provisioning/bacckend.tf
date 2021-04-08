6b67a7f83bc69080b553ba308c601d4896d9fed0
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-08 09:14:02 +0000

    Updated bacckend.tf

--- Resource-Provisioning/bacckend.tf
+++ Resource-Provisioning/bacckend.tf
@@ -1,6 +1,9 @@
 terraform {
   backend "azurerm" {
-    	  
+  storage_account_name = "__terraformstorageaccount__"
+	    container_name       = "terraform"
+	    key                  = "terraform.tfstate"
+	access_key  ="__storagekey__"  	  
 
   }
 }
