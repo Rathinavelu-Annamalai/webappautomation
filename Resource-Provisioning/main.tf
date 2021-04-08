9ebc32d86201cc539b14cf1f383d11b9b5c64178
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-08 03:13:09 +0000

    Updated main.tf

--- Resource-Provisioning/main.tf
+++ Resource-Provisioning/main.tf
@@ -39,13 +39,13 @@ module "application-subnets" {
 }
 
 resource "azurerm_network_security_group" "example" {
-  //name                = "example-nsg"
-  name="${var.nsg_names[count.index]}"
-  count=length(var.nsg_names)
+  name                = "example-nsg"
+ // name="${var.nsg_names[count.index]}"
+  //count=length(var.nsg_names)
   //location            = azurerm_resource_group.dev.location
   location            = "${var.location}"
   //resource_group_name = "${azurerm_resource_group.dev.name}""
-  resource_group_name = "${var.resource_group_name}"
+  resource_group_name = "${azurerm_resource_group.resource_group.name}"
 
   security_rule = [
     {
