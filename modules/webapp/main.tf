b4b67c45033177c08a942475a728e9ea5bd73d5c
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-09 10:09:47 +0000

    Updated main.tf

---
+++ modules/webapp/main.tf
@@ -0,0 +1,130 @@
+resource "azurerm_app_service_plan" "dev" {
+	  //name                = "__appserviceplan__"
+      name                 = "${var.appserviceplan}"
+	  location            =  "${var.location}"
+	  resource_group_name="${var.resource_group_name}"
+	
+	  sku {
+	    tier = "Premiumv2"
+	    size = "P1v2"
+	  }
+	    tags = {
+	    environment = "dev"
+	    createdby="sampath"
+	    modeofdeployment= "azurecicd"
+	  }
+	
+	}
+	
+	resource "azurerm_application_insights" "example" {
+	  //name                = "__appinsights__"
+	  location            =  "${var.location}"
+	  resource_group_name="${var.resource_group_name}"
+	  application_type    = "web"
+	tags = {
+	    environment = "dev"
+	    createdby="sampath"
+	    modeofdeployment= "azurecicd"
+	  }
+	}
+	
+	resource "azurerm_app_service" "dev" {
+	  //name                = "__appservicename__"
+      name                 = "${var.appservicename}"
+	  location            =  "${var.location}"
+	  resource_group_name="${var.resource_group_name}"
+	  app_service_plan_id = "${azurerm_app_service_plan.dev.id}"
+	  depends_on = [azurerm_app_service_plan.dev , azurerm_monitor_autoscale_setting.asplan1]
+	  app_settings = {
+	    "APPINSIGHTS_INSTRUMENTATIONKEY" = "${azurerm_application_insights.example.instrumentation_key}",
+	    "WEBSITE_DNS_SERVER": "168.63.129.16",
+	    "WEBSITE_VNET_ROUTE_ALL": "1"
+	  }
+	
+	    tags = {
+	    environment = "dev"
+	    createdby="sampath"
+	    modeofdeployment= "azurecicd"
+	  }
+	}
+	
+    
+    resource "azurerm_app_service_virtual_network_swift_connection" "vnetintegrationconnection" {
+	  app_service_id  = azurerm_app_service.dev.id
+	  subnet_id       = azurerm_subnet.integrationsubnet.id
+	}
+	
+	
+	
+	resource "azurerm_monitor_autoscale_setting" "asplan1" {
+	  //name                ="__azuremonitor__"
+      name= "${var.azuremonitorname}"
+	  resource_group_name = azurerm_app_service_plan.dev.resource_group_name
+	  location            =  azurerm_app_service_plan.dev.location
+	  target_resource_id  = azurerm_app_service_plan.dev.id
+	
+	  profile {
+	    name = "default"
+	
+	    capacity {
+	      default = 1
+	      minimum = 1
+	      maximum = 2
+	    }
+	
+	    rule {
+	      metric_trigger {
+	        metric_name        = "CpuPercentage"
+	        metric_resource_id = azurerm_app_service_plan.dev.id
+	        time_grain         = "PT1M"
+	        statistic          = "Average"
+	        time_window        = "PT5M"
+	        time_aggregation   = "Average"
+	        operator           = "GreaterThan"
+	        //threshold          = var.cpuUpperThreshold
+	     threshold          = 80
+	      }
+	
+	      scale_action {
+	        direction = "Increase"
+	        type      = "ChangeCount"
+	        value     = "1"
+	        cooldown  = "PT15M"
+	      }
+	    }
+	
+	    rule {
+	      metric_trigger {
+	        metric_name        = "CpuPercentage"
+	        metric_resource_id = azurerm_app_service_plan.dev.id
+	        time_grain         = "PT1M"
+	        statistic          = "Average"
+	        time_window        = "PT5M"
+	        time_aggregation   = "Average"
+	        operator           = "LessThan"
+	        threshold          = 30
+	      }
+	
+	      scale_action {
+	        direction = "Decrease"
+	        type      = "ChangeCount"
+	        value     = "1"
+	        cooldown  = "PT15M"
+	      }
+	    }
+	  }
+	
+	  notification {
+	    email {
+	      send_to_subscription_administrator    = false
+	      send_to_subscription_co_administrator = false
+	      custom_emails                         = ["poorani.kamalakannan@allianzlife.com"]
+	    }
+	  }
+	    depends_on = [azurerm_app_service_plan.dev]
+	    tags = {
+	    environment = "dev"
+	    createdby="poorani"
+	    modeofdeployment= "azurecicd"
+	  }
+	}
