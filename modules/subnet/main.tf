cd3d498f21556a894c2d6f64d278a7e38c4b0ad0
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-09 06:14:53 +0000

    Updated main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -9,7 +9,8 @@
 resource "azurerm_subnet" "subnet" {
   count                = length(var.subnet_names)
   resource_group_name  = "${var.resource_group_name}"
-  name                 = "${var.subnet_names[count.index]}"
+  //name                 = "${var.subnet_names[count.index]}"
+  name                 = "[var.subnet_names[count.index]]"
   virtual_network_name = "${var.vnet_name}"
   //address_prefixes     = "${var.subnet_address_prefixes[count.index]}"
   address_prefixes     = "[var.subnet_address_prefixes[count.index]]"
