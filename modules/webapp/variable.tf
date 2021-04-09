cee93db7cf67f020f57f5cf69a91bf6e5a37002a
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-09 15:03:22 +0000

    Updated variable.tf

--- modules/webapp/variable.tf
+++ modules/webapp/variable.tf
@@ -16,4 +16,10 @@ variable "azuremonitorname" {
 
 variable "location" {}
 variable "resource_group_name" {}
-
+variable "tags" {
+type = map
+default = {
+  environment = "dev-demo"
+  CreatedBy = "terraform"
+  ModeOfDeployment = "Automation"
+}
