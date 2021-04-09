d07320f0cae8c3cb11866d2f62a3d1be889d3511
Poorani Kamalakannan <pooranikamalakannan@allianzlife.com>
2021-04-09 10:48:14 +0000

    Updated main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -68,6 +68,17 @@ terraform {
 	    source_address_prefix      = "*"
 	    destination_address_prefix = "*"
 	  }
+	  {
+	    name                       = "nsgrule2"
+	    priority                   = 100
+	    direction                  = "Inbound"
+	    access                     = "Allow"
+	    protocol                   = "Tcp"
+	    source_port_range          = "*"
+	    destination_port_range     = "80"
+	    source_address_prefix      = "*"
+	    destination_address_prefix = "*"
+	  }
 	   tags = {
 	   environment = "dev"
 	   createdby="poorani"
