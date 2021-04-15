bb5c4a6f7726898d9bf559eced28b9349f809dcb
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-15 04:38:26 +0000

    Added main.tf

---
+++ IFRS9-migration/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 0.12.0"
+}
+provider "azurerm" {
+features {}
+}
+
+module "resource_group" {
+ source ="./modules/resourcegroup"
+ resource_group_name = "${var.prefix}-${var.resource_group_name}"
+ location = "${var.location}"
+}
+
+module "vnet" {
+  source              = "./modules/vnet"
+  resource_group_name = "${module.resource_group.resource_group_name}"
+  location            = "${module.resource_group.location}"
+  vnet_name           = "${var.prefix}-vnet"
+  address_space       = "${var.address_space}"
+}
+
+module "application-subnets" {
+  source              = "./modules/subnet"
+  resource_group_name = "${module.resource_group.resource_group_name}"
+  location            = "${module.resource_group.location}"
+  vnet_name           = "${module.vnet.vnet_name}"
+  subnet_names         ="${var.subnet_names}"
+  subnet_address_prefixes = "${var.subnet_address_prefixes}"
+}
