8445aeca8faea19923724693d6220ecf6f701b49
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-15 03:05:55 +0000

    Updated main.tf

--- modules/IFRS9-Migration/main.tf
+++ modules/IFRS9-Migration/main.tf
@@ -3,3 +3,20 @@ module "resource_group" {
  resource_group_name = "${var.resource_group_name}"
  location = "${var.location}"
 }
+
+module "vnet" {
+  source              = "../modules/vnet"
+  resource_group_name = "${module.resource_group.resource_group_name}"
+  location            = "${module.resource_group.location}"
+  vnet_name           = "${module.resource_group.resource_group_name}-vnet"
+  address_space       = "${var.address_space}"
+}
+
+module "application-subnets" {
+  source              = "../modules/subnet"
+  resource_group_name = "${module.resource_group.resource_group_name}"
+  location            = "${module.resource_group.location}"
+  vnet_name           = "${module.vnet.vnet_name}"
+  subnet_names         ="${var.subnet_names}"
+  subnet_address_prefixes = "${var.subnet_address_prefixes}"
+}
