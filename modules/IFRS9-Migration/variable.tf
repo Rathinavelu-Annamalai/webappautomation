1dbbf1188d6011915715bf8425eec79a8adaa5eb
Sampath Palanisamy <sampathpalanisamy@allianzlife.com>
2021-04-15 03:07:56 +0000

    Added variable.tf

---
+++ modules/IFRS9-Migration/variable.tf
@@ -0,0 +1,48 @@
+variable "resource_group_name" {
+  description = ""
+  default     = ""
+}
+
+variable "location" {
+  description = ""
+  default     = ""
+}
+
+
+variable "address_space" {
+  description = ""
+  default     = ""
+}
+
+variable "subnet_names" {
+  description = ""
+  //type= list
+  //default=[]
+}
+
+variable "subnet_address_prefixes" {
+  description = ""
+  //type= list
+  //default= []
+}
+
+
+/*variable "application" {
+  description = ""
+  default     = ""
+}
+
+variable "environment" {
+  description = ""
+  default     = ""
+}*/
+
+
+variable "tags" {
+type = map
+default = {
+  environment = "dev-demo"
+  CreatedBy = "terraform"
+  ModeOfDeployment = "Automation"
+}
+}
